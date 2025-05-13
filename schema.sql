4a7642769135424caf7db7e0f7c74c942132841b
supermochi0916 <supermochi0916@gmail.com>
2025-05-13 18:53:09 +0900

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS posts;
 CREATE TABLE posts (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,  -- 名前カラムを追加
     text TEXT NOT NULL,
     created_at DATETIME NOT NULL
 );
