2cae06b55d842bfb293a0fe3ab4f5d622c3b153d
supermochi0916 <supermochi0916@gmail.com>
2025-05-13 19:02:56 +0900

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,7 +1,8 @@
 DROP TABLE IF EXISTS posts;
 CREATE TABLE posts (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,  -- 名前カラムを追加
+    name TEXT,
+    password_id TEXT,  -- パスワードから生成したIDカラムを追加
     text TEXT NOT NULL,
     created_at DATETIME NOT NULL
 );
